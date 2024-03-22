3e8e1f1df8094449532e7e42051f0d6888d8b797
micky rendon <elderendon@hotmail.es>
2024-03-22 19:39:37 -0300

    [add]iteracion dinamica de cards

--- src/components/Proyects/cardsContent.js
+++ src/components/Proyects/cardsContent.js
@@ -1,65 +1,72 @@
 export const proyectStructure = [
     // imgAlt =  la primera en mayuscula para que no hayan errores
     {
-        imgAlt: `Todo-list`,
-        bkgImage: `url(./assets/profilePic/gallery/proyectsBackgrounds/todoList.png)`,
-        iconImage: `./assets/profilePic/gallery/ProyectsIcons/todoList.svg`,
-        buttons : {
-            demoURl: `https://mickyrendon.github.io/TODOs_app/`,
-            repoURl: `https://github.com/mickyrendon/TODOs_app`
-
-        },
-        ownClass: `todo-list`,
-    },
-    {
+        title: `SHOPPI`,
+        content: `Ecommerce que permite filtrar productos por su título y categoría, consultar detalles de un producto, agregar productos al carrito de compras, hacer checkout y revisar historial de compras. Usa la API de 'Fake Store'. Componentes, Modularización, local storage, navegación con react-router-dom, react-hooks, refactorización del código (DRY), creado con ReactJS, Vite.js, HTML y Tailwind.`,
         imgAlt: `Shoppi`,
-        bkgImage: `url(./assets/profilePic/gallery/proyectsBackgrounds/shoppi.png)`,
-        iconImage: `./assets/profilePic/gallery/ProyectsIcons/shoppi.svg`,
+        bkgImage: `/assets/proyectsBackgrounds/shoppi.png`,
+        iconImage: `assets/ProyectsIcons/shoppi.svg`,
         buttons : {
             demoURl: `https://shoppiapp.netlify.app/`,
             repoURl: `https://github.com/mickyrendon/shoppi`
-
         },
         ownClass: `shoppi`,
     },
     {
+        title: `TODO LIST`,
+        content: `Aplicación que permite crear tareas pendientes con ReactJS, componentes, props y atributos, props drillin, eventos, estados, librerias de iconos, local storage, custom hooks, features-first, directories en React, useEffect, react context, use context, react portals y formularios.`,
+        imgAlt: `Todo-list`,
+        bkgImage: `assets/proyectsBackgrounds/todoList.png`,
+        iconImage: `assets/ProyectsIcons/todoList.svg`,
+        buttons : {
+            demoURl: `https://mickyrendon.github.io/TODOs_app/`,
+            repoURl: `https://github.com/mickyrendon/TODOs_app`
+        },
+        ownClass: `todo-list`,
+    },
+    {
+        title: `MMOVIE`,
+        content: `Aplicación que permite ver y buscar peliculas en cartelera, usa la API de 'TheMovieDB'. Modularización, local storage, location, hash navigation, manejo profesional del DOM, refactorización del código (DRY), creado con vanilla JavaScript, Axios, HTML, Tailwind y Glide.js.`,
         imgAlt: `Mmovie`,
-        bkgImage: `url(./assets/profilePic/gallery/proyectsBackgrounds/mmovie.png)`,
-        iconImage: `./assets/profilePic/gallery/ProyectsIcons/mmovieLogo.svg`,
+        bkgImage: `assets/proyectsBackgrounds/mmovie.png`,
+        iconImage: `assets/ProyectsIcons/mmovieLogo.svg`,
         buttons : {
             demoURl: `https://mickyrendon.github.io/mmovie/#home`,
             repoURl: `https://github.com/mickyrendon/mmovie`
-
         },
         ownClass: `mmovie`,
     },
     {
+        title: `FIND FOX`,
+        content: `Aplicación que permite buscar imagenes de zorros de forma aleatoria, usa la API de 'randomfox' y buenas prácticas como lazy loader y dinamic import creado con vanilla JavaScript, HTML y Tailwind.`,
         imgAlt: `Find-Fox`,
-        bkgImage: `url(./assets/profilePic/gallery/proyectsBackgrounds/ffox.png)`,
-        iconImage: `./assets/profilePic/gallery/ProyectsIcons/ffox.svg`,
+        bkgImage: `assets/proyectsBackgrounds/ffox.png`,
+        iconImage: `assets/ProyectsIcons/ffox.svg`,
         buttons : {
             demoURl: `https://find-fox.netlify.app/`,
             repoURl: `https://github.com/mickyrendon/FindFox`
-
         },
         ownClass: `fox`,
     },
     {
+        title: `MI RESTAURANT`,
+        content: `Template personalizable para cualquier restaurante. Creado con vanilla JavaScript, HTML, CSS, Tailwind. La pagina permite navegar facilmente al usuario, tiene un botón de acción para que el usuario reserve o pida con una llamada.`,
         imgAlt: `mi-resto`,
-        bkgImage: `url(./assets/profilePic/gallery/proyectsBackgrounds/resto.png)`,
-        iconImage: `./assets/profilePic/gallery/ProyectsIcons/resto.svg`,
+        bkgImage: `assets/proyectsBackgrounds/resto.png`,
+        iconImage: `assets/ProyectsIcons/resto.svg`,
         buttons : {
             demoURl: `https://mirestorant.netlify.app/`,
             repoURl: `https://github.com/mickyrendon/Sushi-resto`
-
         },
         ownClass: `resto-ctr`,
     },
 
     {
+        title: `TWITTER CLONE`,
+        content: `Clon de Twitter del Platzi challenge portfolio web creado con HTML, CSS y un poco de JS con diseño responsive.`,
         imgAlt: `Twitter-clone`,
-        bkgImage: `url(./assets/profilePic/gallery/proyectsBackgrounds/twitter.png)`,
-        iconImage: `./assets/profilePic/gallery/ProyectsIcons/twitter-svgrepo-com.svg`,
+        bkgImage: `assets/proyectsBackgrounds/twitter.png`,
+        iconImage: `assets/ProyectsIcons/twitter-svgrepo-com.svg`,
         buttons : {
             demoURl: `https://mickyrendon.github.io/twitter-clone/`,
             repoURl: `https://github.com/mickyrendon/twitter-clone`
@@ -67,9 +74,11 @@ export const proyectStructure = [
         ownClass: `twtr`,
     },
     {
+        title: `PALE BLUE DOT PODCAST`,
+        content: `Pale Blue Dot Podcast es una pagina web 'responsiva', creada con puro HTML Y CSS, ésta pagina le permite reproducir podcast desde spotify mediante el uso de iframes.`,
         imgAlt: `PaleBlueDotPodcast-logo`,
-        bkgImage: `url(./assets/profilePic/gallery/proyectsBackgrounds/paleBlue.png)`,
-        iconImage: `./assets/profilePic/gallery/logo.svg`,
+        bkgImage: `assets/proyectsBackgrounds/paleBlue.png`,
+        iconImage: `assets/logo.svg`,
         buttons : {
             demoURl: `https://mickyrendon.github.io/PaleBlueDotPodcast/`,
             repoURl: `https://github.com/mickyrendon/PaleBlueDotPodcast`
---
+++ src/components/SkillCards/techsContent.js
@@ -0,0 +1,89 @@
+export const stack = [
+    {
+        name: `React`,
+        url: `./assets/profilePic/techIcons/react.svg`
+    },
+    {
+        name: `JavaScript`,
+        url: `./assets/profilePic/techIcons/js.svg`
+    },
+    {
+        name: `HTML5`,
+        url: `./assets/profilePic/techIcons/html.svg`
+    },
+    {
+        name: `CSS3`,
+        url: `./assets/profilePic/techIcons/css.svg`
+    },
+    {
+        name: `Tailwind`,
+        url: `./assets/profilePic/techIcons/tailwind.svg`
+    },
+    {
+        name: `Bootstrap`,
+        url: `./assets/profilePic/techIcons/bootstrap.svg`
+    },
+    {
+        name: `Sass`,
+        url: `./assets/profilePic/techIcons/sass.svg`
+    },
+    {
+        name: `Git`,
+        url: `./assets/profilePic/techIcons/git.svg`
+    },
+    {
+        name: `Github`,
+        url: `./assets/profilePic/techIcons/github.svg`
+    },
+    {
+        name: `MySQL`,
+        url: `./assets/profilePic/techIcons/mysql.svg`
+    },
+    {
+        name: `Webpack`,
+        url: `./assets/profilePic/techIcons/webpack.svg`
+    },
+    {
+        name: `npm`,
+        url: `./assets/profilePic/techIcons/npm.svg`
+    },
+    {
+        name: `Figma`,
+        url: `./assets/profilePic/techIcons/figma.svg`
+    },
+    {
+        name: `Adobe XD`,
+        url: `./assets/profilePic/techIcons/xd.svg`
+    },
+    {
+        name: `Photoshop`,
+        url: `./assets/profilePic/techIcons/ps.svg`
+    },
+    {
+        name: `Ilustrator`,
+        url: `./assets/profilePic/techIcons/ai.svg`
+    },
+    {
+        name: `Premiere Pro`,
+        url: `./assets/profilePic/techIcons/pr.svg`
+    }
+]
+
+export const currentLearning = [
+    {
+        name: `TypeScript`,
+        url: `./assets/profilePic/techIcons/typescript.svg`
+    },
+    {
+        name: `NodeJS`,
+        url: `./assets/profilePic/techIcons/node.svg`
+    },
+    {
+        name: `MongoDB`,
+        url: `./assets/profilePic/techIcons/mongo.svg`
+    },
+    {
+        name: `Express`,
+        url: `./assets/profilePic/techIcons/express.svg`
+    }
+]
